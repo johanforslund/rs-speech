73bbb218fa872e37b67a1521987c3fdadb29b3a3
johanforslund <johan-forslund@hotmail.com>
2019-03-20 22:11:28 +0100

    fixed bug where popup tried to call getBackgroundPage before background was loaded

--- popup.js
+++ popup.js
@@ -1,6 +1,10 @@
 let playPauseButton = document.getElementById('playPause');
-let playing = chrome.extension.getBackgroundPage().isPlaying();
-playPauseButton.childNodes[0].innerHTML = playing ? "pause" : "play_arrow";
+let playing = false;
+
+document.addEventListener('DOMContentLoaded',function(){
+  playing = chrome.extension.getBackgroundPage().isPlaying();
+  playPauseButton.childNodes[0].innerHTML = playing ? "pause" : "play_arrow";
+})
 
 playPauseButton.onclick = function(element) {
   chrome.extension.sendMessage({ msg: "playPause" }, function(response) {
