c738a8d940e61175e5ffcad135a9f537054e9078
johanforslund <johan-forslund@hotmail.com>
2019-03-23 10:25:46 +0100

    fixed bug where background page goes inactive

--- popup.js
+++ popup.js
@@ -1,10 +1,12 @@
 let playPauseButton = document.getElementById('playPause');
-let playing = false;
+let playing;
 
 document.addEventListener('DOMContentLoaded',function(){
-  playing = chrome.extension.getBackgroundPage().isPlaying();
-  playPauseButton.childNodes[0].innerHTML = playing ? "pause" : "play_arrow";
-})
+  chrome.runtime.getBackgroundPage(background => {
+    playing = background.isPlaying();
+    playPauseButton.childNodes[0].innerHTML = playing ? "pause" : "play_arrow";
+  });
+});
 
 playPauseButton.onclick = function(element) {
   chrome.extension.sendMessage({ msg: "playPause" }, function(response) {
